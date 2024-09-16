503f8adcb38c5384dd3d1e72d7dd2864f76ef1ba
Neno Craig <shaeneno.craig.walmart@gmail.com>
2024-09-15 22:31:02 -0500

    added and deleted files

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
 let playerWins = 0;
-let computerWins = 0;
+    let computerWins = 0;
 
-function playGame(playerChoice) {
-    fetch('/play', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({choice: playerChoice})
-    })
-    .then(response => response.json())
-    .then(data => {
-        const computerChoice = data.computer_choice;
-        const result = data.result;
-        
-        document.getElementById("result").innerHTML = `Computer chose: ${computerChoice}<br>`;
-        
-        if (result === "Player") {
+    function playGame(playerChoice) {
+        const choices = ["rock", "paper", "scissors"];
+        const computerChoice = choices[Math.floor(Math.random() * 3)];
+        let resultMessage = `Computer chose: ${computerChoice}<br>`;
+
+        if ((playerChoice === "rock" && computerChoice === "scissors") || 
+            (playerChoice === "scissors" && computerChoice === "paper") || 
+            (playerChoice === "paper" && computerChoice === "rock")) {
             playerWins++;
-            document.getElementById("result").innerHTML += "You won this round!";
-        } else if (result === "Computer") {
-            computerWins++;
-            document.getElementById("result").innerHTML += "Computer won this round!";
+            resultMessage += "You won this round!";
+        } else if (playerChoice === computerChoice) {
+            resultMessage += "It's a tie!";
         } else {
-            document.getElementById("result").innerHTML += "It's a tie!";
+            computerWins++;
+            resultMessage += "Computer won this round!";
         }
 
-        document.getElementById("result").innerHTML += `<br>Player: ${playerWins}, Computer: ${computerWins}`;
+        resultMessage += `<br>Player: ${playerWins}, Computer: ${computerWins}`;
 
         if (playerWins === 2 || computerWins === 2) {
             if (playerWins > computerWins) {
-                document.getElementById("result").innerHTML += "<br><strong>Congratulations! You won the best of three.</strong>";
+                resultMessage += "<br><strong>Congratulations! You won the best of three.</strong>";
             } else {
-                document.getElementById("result").innerHTML += "<br><strong>Computer won the best of three. Better luck next time!</strong>";
+                resultMessage += "<br><strong>Computer won the best of three. Better luck next time!</strong>";
             }
 
             // Reset the game
             playerWins = 0;
             computerWins = 0;
         }
-    });
-}
+
+        document.getElementById("result").innerHTML = resultMessage;
+    }
+
+
